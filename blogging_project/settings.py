45597fe40998a82e33ba0add51bb0ee5e7e3591d
sheik-kajaalimohiddin <sheik.kajaalimohiddin359@gmail.com>
2022-08-13 19:41:31 -0500

    removed secret key and email info as it is will be exposed to public

--- blogging_project/settings.py
+++ blogging_project/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)$f*z!yv$sxr(jn6um4dt@&o8js1qcn7zlc2&s2o-7g3kat_7#'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -133,20 +133,6 @@ LOGIN_URL = 'login'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# email settings
-#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-#EMAIL_HOST = 'smtp.office365.com'
-#EMAIL_PORT = 587
-#EMAIL_HOST_USER = 'sheik.kajaalimohiddin359@outlook.com'
-#EMAIL_HOST_PASSWORD = 'Mohiddin@1234'
-#EMAIL_USE_TLS = True
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'raisingraju.143@gmail.com'
-EMAIL_HOST_PASSWORD = 'cmimmatzhpowtrbb'
-#EMAIL_HOST_PASSWORD = 'qnmawhuweossgcid' - localhost
-EMAIL_USE_TLS = True
 
 django_heroku.settings(locals())
